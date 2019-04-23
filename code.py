bf7392926f002a4f9edb1f90da7ca4b41ede4e57
Mrunali Kadam <37960113+mrunalika@users.noreply.github.com>
2019-04-23 14:04:20 -0700

    Create code.py

---
+++ code.py
@@ -0,0 +1,125 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets('MNIST_data', one_hot = True)
+
+n_nodes_hl1 = 500
+n_nodes_hl2 = 500
+n_nodes_hl3 = 500
+
+n_classes = 10
+batch_size = 100
+
+x = tf.placeholder('float', [None, 784])
+y = tf.placeholder('float')
+
+""""def TRAIN_SIZE(num):
+    print ('Total Training Images in Dataset = ' + str(mnist.train.images.shape))
+    print ('--------------------------------------------------')
+    x_train = mnist.train.images[:num,:]
+    print ('x_train Examples Loaded = ' + str(x_train.shape))
+    y_train = mnist.train.labels[:num,:]
+    print ('y_train Examples Loaded = ' + str(y_train.shape))
+    print('')
+    return x_train, y_train
+
+def TEST_SIZE(num):
+    print ('Total Test Examples in Dataset = ' + str(mnist.test.images.shape))
+    print ('--------------------------------------------------')
+    x_test = mnist.test.images[:num,:]
+    print ('x_test Examples Loaded = ' + str(x_test.shape))
+    y_test = mnist.test.labels[:num,:]
+    print ('y_test Examples Loaded = ' + str(y_test.shape))
+    return x_test, y_test
+
+def display_digit(num):
+    print(y_train[num])
+    label = y_train[num].argmax(axis=0)
+    image = x_train[num].reshape([28,28])
+    plt.title('Example: %d  Label: %d' % (num, label))
+    plt.imshow(image, cmap=plt.get_cmap('gray_r'))
+    plt.show()
+    
+def display_mult_flat(start, stop):
+    images = x_train[start].reshape([1,784])
+    for i in range(start+1,stop):
+        images = np.concatenate((images, x_train[i].reshape([1,784])))
+    plt.imshow(images, cmap=plt.get_cmap('gray_r'))
+    plt.show()
+
+def weight_variable(name, shape):
+    initer = tf.truncated_normal_initializer(stddev=0.01)
+    return tf.get_variable('W_' + name,
+                           dtype=tf.float32,
+                           shape=shape,
+                           initializer=initer)
+def bias_variable(name, shape):
+    initial = tf.constant(0., shape=shape, dtype=tf.float32)
+    return tf.get_variable('b_' + name,
+                           dtype=tf.float32,
+                           initializer=initial)
+
+
+x_train, y_train = TRAIN_SIZE(55000)
+
+display_digit(ran.randint(0, x_train.shape[0]))
+
+display_mult_flat(0,400)"""
+
+def neural_network_model(data):
+    hidden_1_layer = {'weights':tf.Variable(tf.random_normal([784, n_nodes_hl1])),
+                      'biases':tf.Variable(tf.random_normal([n_nodes_hl1]))}
+
+    hidden_2_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
+                      'biases':tf.Variable(tf.random_normal([n_nodes_hl2]))}
+
+    hidden_3_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
+                      'biases':tf.Variable(tf.random_normal([n_nodes_hl3]))}
+
+    output_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
+                    'biases':tf.Variable(tf.random_normal([n_classes])),}
+
+
+    l1 = tf.add(tf.matmul(data,hidden_1_layer['weights']), hidden_1_layer['biases'])
+    l1 = tf.nn.relu(l1)
+
+    l2 = tf.add(tf.matmul(l1,hidden_2_layer['weights']), hidden_2_layer['biases'])
+    l2 = tf.nn.relu(l2)
+
+    l3 = tf.add(tf.matmul(l2,hidden_3_layer['weights']), hidden_3_layer['biases'])
+    l3 = tf.nn.relu(l3)
+
+    output = tf.matmul(l3,output_layer['weights']) + output_layer['biases']
+
+    return output
+
+def train_neural_network(x):
+    prediction = neural_network_model(x)
+    # OLD VERSION:
+    #cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(prediction,y) )
+    # NEW:
+    cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y) )
+    optimizer = tf.train.AdamOptimizer().minimize(cost)
+    
+    hm_epochs = 3
+    with tf.Session() as sess:
+        # OLD:
+        #sess.run(tf.initialize_all_variables())
+        # NEW:
+        sess.run(tf.global_variables_initializer())
+
+        for epoch in range(hm_epochs):
+            epoch_loss = 0
+            for _ in range(int(mnist.train.num_examples/batch_size)):
+                epoch_x, epoch_y = mnist.train.next_batch(batch_size)
+                _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
+                epoch_loss += c
+
+            print('Epoch', epoch, 'completed out of',hm_epochs,'loss:',epoch_loss)
+
+        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+
+        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+        print('Training Accuracy:',accuracy.eval({x:mnist.train.images, y:mnist.train.labels}))
+        print('Testing Accuracy:',accuracy.eval({x:mnist.test.images, y:mnist.test.labels}))
+
+train_neural_network(x)
